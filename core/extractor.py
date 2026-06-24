8ec245975ea1f577847132b3b2f440a245eb103b
SIDHARTH JAIN <jisidharthjain@gmil.com>
2026-06-25 01:16:58 +0530

    Implemented Vector Store Feature

---
+++ core/extractor.py
@@ -0,0 +1,47 @@
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough , RunnableLambda
+import os
+
+def get_llm() :
+    return ChatMistralAI(model="mistral-small-latest" , mistal_api_key = os.getenv("MISTRAL_API_KEY"))
+
+def build_chain(system_prompt : str):
+    llm=get_llm()
+    return (RunnablePassthrough() | RunnableLambda(lambda x: {"text": x}) | ChatPromptTemplate.from_messages([
+        (
+            "system" , system_prompt , 
+
+        )
+        ("human" , {"text"})
+    ]) | llm | StrOutputParser())
+
+def extract_action_items(transcript:str)->str:
+    chain=build_chain("You are an expert meeting analyst , from the meeting transcript extract all action items"
+                      "for each provide:" \
+                      "task description" \
+                      "who is responsible" \
+                      "fromat as a numbered list , if none found ssay no action items were found"
+                      )
+    
+    return chain.invoke(transcript)
+
+def extract_key_decision(transcript:str)->str:
+    chain=build_chain("from the meeting transcript extract key decisions made" \
+    "format as a numbered list   if none found say no decisions found" \
+                      
+                      )
+    
+    return chain.invoke(transcript)
+
+def extract_questions(transcript:str)->str:
+    chain=build_chain("from the meeting transcript extract all unresolved questions" \
+    "or topics needing folllow up format as a numbered list" \
+    "if none found say no questions found" \
+                      
+                      )
+    
+    return chain.invoke(transcript)
+
+ 
---
+++ core/summarize.py
@@ -0,0 +1,62 @@
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.runnables import RunnablePassthrough , RunnableLambda
+
+import os
+
+def get_llm() :
+    return ChatMistralAI(model="mistral-small-latest" , mistal_api_key = os.getenv("MISTRAL_API_KEY"))
+
+def split_transcript(transcript:str)->list :
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size= 3000 ,
+        chunk_overlap=200
+    )
+
+    return splitter.split_text(transcript)
+
+def summarize(transcript : str)->str :
+    llm = get_llm()
+
+    map_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system","Summarize this portion of the transcript precisely"),
+            ("human", "{text}")
+        ]
+    )
+
+    map_chain = map_prompt | llm | StrOutputParser()
+
+    chunks = split_transcript(transcript)
+
+    chunk_summaries = [map_chain.invoke ({"text": chunk}) for chunk in chunks]
+
+    combined = "\n\n".join(chunk_summaries)
+
+    combined_prompt = ChatPromptTemplate.from_messages([(
+        "system","You are an expert meeting summarizer Combine these partial summaries"
+        "into one final professional summary"
+    ),
+    ("human","{text}")])
+
+    combined_chain= (RunnablePassthrough()| RunnableLambda(lambda x:{"text": x}) | combined_prompt | llm | StrOutputParser)
+
+    return combined_chain.invoke(combined)
+
+def generate_title(transcript:str)->str:
+    llm=get_llm()
+    title_chain=(RunnablePassthrough | RunnableLambda(lambda x:{"text": x})| ChatPromptTemplate.from_messages([(
+        "system" , "Based in the meeting transcription , generate a short professional meeting title ",
+        "only return the title nothing else",
+
+    )
+    ("human","{text}")])|llm | StrOutputParser())
+
+    return title_chain.invoke(transcript[:2000])
+
+
+
+
+
